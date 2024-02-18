0e7275303a71dfaba6e428ef90a54a1c662bd39e
Jumpei-Arigaya <junpeeei0407@gmail.com>
2024-02-18 23:45:27 +0900

    remove unnecessary import

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use App\Models\Sanctum\PersonalAccessToken;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
